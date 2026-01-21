f5a1bad1d858bc0bb5dac4152f4fbb360f86b3e3
iam-anupa123 <anupavitharana@gmail.com>
2026-01-21 21:17:46 +0530

    final fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      css: {
+        additionalData: ``,
+      },
+    },
+  },
   server: {
     port: 3000,
     strictPort: false,
