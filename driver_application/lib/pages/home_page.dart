b8ba3e9d747c396b83cb5e1e822634b0be11df42
Sahan Baddegama <sahaneduonline@gmail.com>
2026-01-29 11:15:11 +0530

    Add Google Map integration to HomePage with initial camera position

--- driver_application/lib/pages/home_page.dart
+++ driver_application/lib/pages/home_page.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
+import 'package:driver_application/global/global_var.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../widgets/side_menu.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,9 +13,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final Completer<GoogleMapController> googleMapsCompleterController = Completer<GoogleMapController>();
+  GoogleMapController? controllerGoogleMap;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            myLocationEnabled: true,
+            initialCameraPosition: googlePlexInitialPosition,
+            onMapCreated: (GoogleMapController mapController){
+              controllerGoogleMap = mapController;
+              googleMapsCompleterController.complete(controllerGoogleMap);
+            },
+          ),
+        ],
+      ),
       drawer: const SideMenu(),
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 255, 59, 59),
