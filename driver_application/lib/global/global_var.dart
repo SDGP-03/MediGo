f74c59e021aebe270c79f43a74d7f7d0d0251b19
Kavinu Thepul <kavinuthepul456@gmail.com>
2026-01-30 20:33:25 +0530

    Update initial camera position for Google Maps to reflect correct coordinates

--- driver_application/lib/global/global_var.dart
+++ driver_application/lib/global/global_var.dart
@@ -5,6 +5,6 @@ String userName = "";
 String googleMapKey = "AIzaSyAxnqf_BBneyllk_WK2ByAsCsSdgH-qkOs";
 
 const CameraPosition googlePlexInitialPosition = CameraPosition(
-  target: LatLng(37.42796133580664, -122.085749655962),
-  zoom: 14.4746,
+  target: LatLng(6.9271, 79.8612),
+  zoom: 14.0,
 );
