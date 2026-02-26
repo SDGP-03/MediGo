65fd300c5d05b34f5f8ec36b61a1813173d7cc96
Kavinu Thepul <kavinuthepul456@gmail.com>
2026-02-26 19:59:54 +0530

    Add ScrollIfNeeded widget for dynamic scrolling behavior

--- driver_application/lib/methods/common_methods.dart
+++ driver_application/lib/methods/common_methods.dart
@@ -2,6 +2,101 @@ import 'dart:io';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class ScrollIfNeeded extends StatefulWidget {
+  const ScrollIfNeeded({
+    super.key,
+    this.padding,
+    this.controller,
+    this.fillViewport = false,
+    required this.child,
+  });
+
+  final EdgeInsetsGeometry? padding;
+  final ScrollController? controller;
+  final bool fillViewport;
+  final Widget child;
+
+  @override
+  State<ScrollIfNeeded> createState() => _ScrollIfNeededState();
+}
+
+class _ScrollIfNeededState extends State<ScrollIfNeeded> {
+  double _contentExtent = 0;
+
+  void _onContentSizeChanged(Size size) {
+    final newExtent = size.height;
+    if (!mounted) return;
+    if (newExtent == _contentExtent) return;
+    setState(() => _contentExtent = newExtent);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final viewportExtent = constraints.maxHeight;
+        final shouldScroll = _contentExtent > viewportExtent + 0.5;
+
+        Widget content = _MeasureSize(
+          onChange: _onContentSizeChanged,
+          child: widget.child,
+        );
+
+        if (widget.fillViewport) {
+          content = ConstrainedBox(
+            constraints: BoxConstraints(minHeight: viewportExtent),
+            child: content,
+          );
+        }
+
+        return SingleChildScrollView(
+          controller: widget.controller,
+          physics: shouldScroll ? null : const NeverScrollableScrollPhysics(),
+          padding: widget.padding,
+          child: content,
+        );
+      },
+    );
+  }
+}
+
+typedef _OnWidgetSizeChange = void Function(Size size);
+
+class _MeasureSize extends SingleChildRenderObjectWidget {
+  const _MeasureSize({required this.onChange, required super.child});
+
+  final _OnWidgetSizeChange onChange;
+
+  @override
+  RenderObject createRenderObject(BuildContext context) =>
+      _RenderMeasureSize(onChange);
+
+  @override
+  void updateRenderObject(
+    BuildContext context,
+    covariant _RenderMeasureSize renderObject,
+  ) {
+    renderObject.onChange = onChange;
+  }
+}
+
+class _RenderMeasureSize extends RenderProxyBox {
+  _RenderMeasureSize(this.onChange);
+
+  _OnWidgetSizeChange onChange;
+  Size? _oldSize;
+
+  @override
+  void performLayout() {
+    super.performLayout();
+    final newSize = child?.size ?? Size.zero;
+    if (_oldSize == newSize) return;
+    _oldSize = newSize;
+    WidgetsBinding.instance.addPostFrameCallback((_) => onChange(newSize));
+  }
+}
 
 class CommonMethods {
   Future<bool> checkConnectivity(BuildContext context) async {
